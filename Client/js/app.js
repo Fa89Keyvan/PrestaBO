18fb047be1db9e8bb8c992444c90d52cb258d820
k.fathabadi <k.fathabadi@DESKTOP-1VMUBML>
2018-08-21 15:50:18 +0430

    970530 1550

---
+++ Client/js/app.js
@@ -0,0 +1,47 @@
+﻿var baseUrl = 'http://localhost/PrestaBO/Server/Api/';
+
+$(document).ready(function () {
+
+    $('#plcSideBar').load('SideBar.html');
+
+    toLastPage();
+    
+});
+
+$(document).on('click', 'a', function () {
+
+    var pageName = $(this).attr('data-url');
+    if (pageName === undefined)
+        return;
+    loadPage(pageName);
+
+});
+
+function toLastPage() {
+    var lastPage = window.localStorage.getItem('lastPage');
+    if (lastPage === '' || lastPage === undefined)
+        loadPage('Dashboard.html')
+    else
+        loadPage(lastPage);
+}
+
+/**
+ *
+ * @param String pageName
+ */
+function loadPage(pageName) {
+    $('.panel').fadeToggle(750);
+    window.localStorage.setItem('lastPage', pageName);
+    $('#plcBody').load(pageName);
+    $('.panel').fadeToggle(750).stop(false,true);
+}
+
+/**
+ * 
+ * @param String headingText
+ */
+function setPanelHeading(headingText) {
+
+    $('.panel-heading').text(headingText);
+
+}
